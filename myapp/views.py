a24f2c5270a162de93955c1f4f0981924b821e22
TREYRERI <kholovamunavvara00@gmail.com>
2026-09-26 04:55:22 +0500

    ProductCreateView

--- myapp/views.py
+++ myapp/views.py
@@ -28,3 +28,24 @@ class ProductDetailView(DetailView):
         context['message'] = 'This is a product detail page'
         # 3 Возвращаем обновленный словарь
         return context
+
+
+from django.views.generic import ListView, DetailView, CreateView
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'myapp/product_form.html'
+
+    def form_valid(self, form):
+        print('Form is valid')
+        return super().form_valid(form) # POST - form - is_valid() - YES - form_valid()
+
+    def form_invalid(self, form):
+        print('Form has errors')
+        print(form.errors)
+        return super().form_invalid(form)
+
+    def get_form(self, form_class = None):
+        form = super().get_form(form_class)
+        form.fields['name'].required = False
+        return form
